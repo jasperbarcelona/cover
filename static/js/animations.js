299dad1236259cbd81a9dbb1b029bb7554e4307f
Jan Paolo Armamento <barcelona.jasperoliver@gmail.com>
2017-04-20 01:29:23 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -4,22 +4,22 @@ onScrollInit( $('.below') );
 onScrollInit( $('.advantages'), $('#page5') );
 
 if (document.documentElement.clientWidth > 1000) {
-        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-180));
+        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-200));
     }
     else{
-        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-140));
+        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-170));
     }
 
 $(window).resize(function() {
     if (document.documentElement.clientWidth > 1000) {
-        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-180));
+        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-200));
     }
     else{
-        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-140));
+        $('#page1-pic-container').css('height',($('#page1-pic-container').width()-170));
     }
 });
 
-$(window).on('scroll', function(){
+/*$(window).on('scroll', function(){
     if (document.documentElement.clientWidth > 1000){
         st = $('body').scrollTop();
         if(st >= 200){
@@ -29,7 +29,7 @@ $(window).on('scroll', function(){
             $("#floating-header").css("top","-80px");
         }
     }
-});
+});*/
 
 function onScrollInit( items, trigger ) {
   items.each( function() {
@@ -53,5 +53,43 @@ function onScrollInit( items, trigger ) {
     });
   });
 }
+
+$(window).on('scroll', function(){
+    var st = $(this).scrollTop();
+    if(st >= 80){
+        $('.blue-overlay').css('opacity', (st/$('.page1-content').height()) - .1);
+    }
+
+    if(st >= $('.page1-content').height()/2){
+        $('#whiteHeader').fadeIn();
+    }
+    else{
+        $('#whiteHeader').fadeOut();
+    }
+
+    if(st == 0){
+        $('.blue-overlay').css('opacity', 0);
+    }
+
+
+
+    if((st >= $('.page1-content').height()) && (st < $('.page1-content').height() * 5)){
+        $('.floating-nav-opt').removeClass('active');
+        $('.white-nav-opt').removeClass('active');
+        $('.featuresNav').addClass('active');
+    }
+
+    else if(st < $('.page1-content').height()){
+        $('.floating-nav-opt').removeClass('active');
+        $('.white-nav-opt').removeClass('active');
+        $('.homeNav').addClass('active');
+    }
+
+    else if(st >= $('.page1-content').height() * 5){
+        $('.floating-nav-opt').removeClass('active');
+        $('.white-nav-opt').removeClass('active');
+        $('.contactNav').addClass('active');
+    }
+});
 
 })
