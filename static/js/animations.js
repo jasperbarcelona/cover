68bf63e0b2b451b455f4b491e14bc0c34d191d3b
Jasper Barcelona <barcelona@jasperoliver@gmail.com>
2017-08-24 02:40:13 +0800

    laptop turnover

--- static/js/animations.js
+++ static/js/animations.js
@@ -19,17 +19,26 @@ $(window).resize(function() {
     }
 });
 
-/*$(window).on('scroll', function(){
-    if (document.documentElement.clientWidth > 1000){
-        st = $('body').scrollTop();
-        if(st >= 200){
-            $("#floating-header").css("top","0");
-        }
-        else if (st<200){
-            $("#floating-header").css("top","-80px");
-        }
-    }
-});*/
+$(window).load(function() {
+    var osElement = $('.banner-text-container'),
+        osAnimationClass = osElement.attr('data-os-animation'),
+        osAnimationDelay = osElement.attr('data-os-animation-delay');
+ 
+    osElement.css({
+        '-webkit-animation-delay':  osAnimationDelay,
+        '-moz-animation-delay':     osAnimationDelay,
+        'animation-delay':          osAnimationDelay
+    });
+ 
+    var osTrigger = ( trigger ) ? trigger : osElement;
+ 
+    osTrigger.waypoint(function() {
+        osElement.addClass('animated').addClass(osAnimationClass);
+    },{
+        triggerOnce: true,
+        offset: '50%'
+    });
+});
 
 function onScrollInit( items, trigger ) {
   items.each( function() {
